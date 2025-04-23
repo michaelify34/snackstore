5ac5c936ad0b992706d22f0ed398f9be4744f8c4
Michael <michaelrharding34@gmail.com>
2025-04-23 17:11:21 -0400

    create classes for drinks

--- store.py
+++ store.py
@@ -17,3 +17,11 @@ class Scootles(Snack):
 class Drink(ABC):
     def __init__(self, price):
         self.price = price
+
+class MrPepper(Drink):
+    def __init__(self):
+        self.name = "MrPepper"
+
+class Brite(Drink):
+    def __init__(self):
+        self.name = "Brite"
