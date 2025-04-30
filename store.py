47e971433485e2155b54dd0c725cb0fc8aab1897
Michael <michaelrharding34@gmail.com>
2025-04-29 23:25:35 -0400

    update comments + spacing

--- store.py
+++ store.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 
-#Snacks
+#Snack classes
 class Snack(ABC):
     pass
 
@@ -31,7 +31,7 @@ class Scootles(Snack):
         return self._price
 
 
-#Drinks
+#Drink classes
 class Drink(ABC):
     pass
 
@@ -62,30 +62,35 @@ class Brite(Drink):
         return self._price
 
 
-#Generate store inventory
+#Handles store inventory
 class Stock():
     def __init__(self):
         self.goods = []
         self.goods_list = [MandN(), Scootles(), MrPepper(), Brite()]
         
+    #Create selection of snacks
     def generate_stock(self):
         for good in self.goods_list:
             self.goods.append(good)
         return self.goods
 
+#Handles user interaction with interface
 class User():
     def __init__(self):
         self._balance = 10
         self._items = []
 
+    #Balance getter
     @property
     def get_balance(self):
         return (f"Balance: ${self._balance}")
     
+    #Items getter
     @property
     def get_items(self):
         return self._items
     
+    #Handles logic of user purchasing item
     def buy_item(self, item):
         if (self._balance - item.price) < 0:
             print("Insufficient funds")
@@ -94,6 +99,7 @@ class User():
             self._balance -= item.price
             self._items.append(item)
 
+#Visualizes interface to allow user to interact
 class Menu():
     @staticmethod
     def store_run():
@@ -107,29 +113,38 @@ class Menu():
             """))
 
 
-# u = User()
-# print(u.get_balance)
-
+#Runtime
 if __name__ == "__main__":
+    #Initialize necessary instances
     s = Stock()
     s.generate_stock()
     u = User()
 
+    #While loop runs the visualization until the user exits
     loop = True
     while loop == True:
         Menu.store_run()
         user_selection = str(input("Selection: "))
 
+            #User Exit
         if user_selection == "Q":
             print("Thank you for visiting Michael's snack store!")
             loop = False
+
+            #View Available Items
         elif user_selection == "V":
             for i, good in enumerate(s.goods):
                 print(f"{good.name}, ${good.price}, id: {i}")
+
+            #Display Balance
         elif user_selection == "B":
             print(u.get_balance)
+            
+            #User Buy Item
         elif user_selection.isnumeric():
             u.buy_item(s.goods[int(user_selection)])
+
+            #Display User's Items
         elif user_selection == "E":
             if len(u._items) == 0:
                 print("You haven't bought anything yet!")
@@ -138,5 +153,7 @@ if __name__ == "__main__":
                 for item in u.get_items:
                     itemlist.append(item.name)
                 print(f"Items: {', '.join(_ for _ in itemlist)}")
+            
+        #Handles unexpected input
         else:
             print("Please make a valid selection.")
